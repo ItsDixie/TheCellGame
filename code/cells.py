edb25b01fa2017ac3361d465a12a97668ebea778
ItsDixie <hipster.yt89@mail.ru>
2023-08-18 19:24:21 +0300

    medium update

--- code/cells.py
+++ code/cells.py
@@ -1,19 +1,62 @@
 
 class cell:
-    def __init__(self, hp, energy, pos, direction, perceptron):
-        self.hp = hp
+    def __init__(self, energy, pos, direction, perceptron, alive_cells, rows, cols):
         self.energy = energy
         self.pos = pos
         self.brain = perceptron
-        self.dir = direction
+        self.dir = direction # от 1 до 8, клетки вокруг бота
+
+        self.cells = alive_cells
+        self.rows = rows
+        self.cols = cols
     
     def think(self):
-        inputs = [self.hp, ]
-        self.brain.feedforward(inputs)
-    
-    def looking(self):
-        pass
-
+        target = self.looking(False)
+        simmilar = self.looking(True)
+        inputs = [self.hp, target, self.dir, simmilar, self.dir]
+        output = self.brain.feedforward(inputs)
+        # [1, 0, 0, 0, 0] - пример
+        #  индекс 0 - движение по направлению, индекс 1 - сменить направление по часовой стрелке (вперед-диагональ-направо)
+        #  индекс 2 - фотосинтезировать энергию, индекс 3 - размножение, индекс 4 - кушать клетку по направлению
+    def looking(self, looking_for_simmilar): # 0 если пустая клетка, 0,5 если край карты, 1 если с ботом
+        x,y = self.pos
+        if(self.dir == 0.125):
+            ox,oy = x,y+1
+        if(self.dir == 0.25):
+            ox,oy = x+1,y+1
+        if(self.dir == 0.375):
+            ox,oy = x+1,y
+        if(self.dir == 0.5):
+            ox,oy = x+1,y-1
+        if(self.dir == 0.625):
+            ox,oy = x,y-1
+        if(self.dir == 0.75):
+            ox,oy = x-1,y-1
+        if(self.dir == 0.875):
+            ox,oy = x-1,y
+        if(self.dir == 1):
+            ox,oy = x-1,y+1
+        
+        if(ox > self.rows):
+            ox = self.rows
+            return 0.5
+        elif(ox < 0):
+            ox = 0
+            return 0.5
+        
+        if(oy > self.cols):
+            oy = self.cols
+            return 0.5
+        elif(oy < 0):
+            oy = 0
+            return 0.5
+        
+        if((ox,oy) in self.cells):
+            if(looking_for_simmilar):
+                return int(any(cell.brain == self.brain for cell in self.cells.values()))
+        else:
+            return 0
+        
     def move(self):
         pass
     def rotate(self):
--- code/graphics.py
+++ code/graphics.py
@@ -3,20 +3,63 @@ import sys
 def run(q):
     pygame.init()
     
+    class IntegerInputField:
+        def __init__(self, x, y, width, height):
+            self.rect = pygame.Rect(x, y, width, height)
+            self.color = (255,255,255)
+            self.text = ""
+            self.font = pygame.font.Font(None, 36)
+
+        def draw(self, surface):
+            pygame.draw.rect(surface, self.color, self.rect)
+            text_surface = self.font.render(self.text, True, (0,0,0))
+            text_rect = text_surface.get_rect(midleft=self.rect.move(0, 20).topleft)
+            surface.blit(text_surface, text_rect)
+
+        def add_digit(self, digit):
+            self.text += digit
+
+        def remove_digit(self):
+            self.text = self.text[:-1]
+
+        def get_value(self):
+            try:
+                return int(self.text)
+            except ValueError:
+                return 0
+    class Button:
+        def __init__(self, x, y, width, height, text):
+            self.rect = pygame.Rect(x, y, width, height)
+            self.color = (0,0,0)
+            self.text = text
+            self.font = pygame.font.Font(None, 36)
+
+        def draw(self, surface):
+            pygame.draw.rect(surface, self.color, self.rect)
+            text_surface = self.font.render(self.text, True, (255,255,255))
+            text_rect = text_surface.get_rect(center=self.rect.center)
+            surface.blit(text_surface, text_rect)
+
     writeCells = False
+    paused = False
 
     CELL_SIZE = 10
     ROWS = 100
     COLS = 100
-    WINDOW_SIZE = (CELL_SIZE * COLS, CELL_SIZE * ROWS)
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    alive_cells = []
+    #WINDOW_SIZE = (CELL_SIZE * COLS, CELL_SIZE * ROWS)
+    WINDOW_SIZE = (1920, 1000)
+    clock = pygame.time.Clock()
+    FPS = 30
+
+    alive_cells = {}
     q.append([ROWS, COLS])
 
-    print(q)
+    input_field = IntegerInputField(1600, 200, 200, 50)
+    button = Button(1600, 10, 100, 50, "Pause")
+    unpause = Button(960, 500, 100, 50, "Paused")
+
     screen = pygame.display.set_mode(WINDOW_SIZE)
-    pygame.display.set_caption("Clickable 2D Canvas")
+    pygame.display.set_caption("CellSimulator")
     cell_pos = {}
 
     def parse_cells():
@@ -34,29 +77,53 @@ def run(q):
     running = True
 
     while running:
-        try:
-            alive_cells = q[1]
-        except Exception: pass
+        clock.tick(FPS)
+    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                get_pos()
-        screen.fill(WHITE)
+                if button.rect.collidepoint(event.pos) and not paused:
+                    paused = True
+                elif(unpause.rect.collidepoint(event.pos) and paused):
+                    paused = False
+                else:
+                    get_pos()
+            elif event.type == pygame.KEYDOWN:
+                if event.unicode.isdigit():
+                    input_field.add_digit(event.unicode)
+                elif event.key == pygame.K_BACKSPACE:
+                    input_field.remove_digit()
+                
+        screen.fill((16, 24, 32))
 
-        if writeCells :
-            for row in range(ROWS):
-                for col in range(COLS):
-                    pygame.draw.rect(screen, BLACK, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
-                    
-        for cell in range(len(alive_cells)):
-            x, y = alive_cells[cell].pos
-            draw_filled_circle(x, y, 5, (255,0,0))
-        def get_pos():
-            x, y = pygame.mouse.get_pos()
-            cell_x = x // CELL_SIZE
-            cell_y = y // CELL_SIZE
-            print(f"Clicked cell at ({cell_x}, {cell_y})")
+        if not paused:
+
+            try:
+                alive_cells = q[1]
+            except Exception: pass
+
+            if writeCells:
+                for row in range(ROWS):
+                    for col in range(COLS):
+                        pygame.draw.rect(screen, (100,100,100), (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
+                        
+            for cell in alive_cells.keys():
+                x, y = alive_cells[cell].pos
+                draw_filled_circle(x, y, 5, (255,0,0))
+                
+            def get_pos():
+                x, y = pygame.mouse.get_pos()
+                cell_x = x // CELL_SIZE
+                cell_y = y // CELL_SIZE
+                print(f"Clicked cell at ({cell_x}, {cell_y})")
+
+            input_field.draw(screen)
+            button.draw(screen)
+        if paused:
+            pause_font = pygame.font.Font(None, 72)
+            unpause.draw(screen)
+        
         pygame.display.flip()
     pygame.quit()
     sys.exit()
